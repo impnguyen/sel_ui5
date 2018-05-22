b506835d12ca2568d282b680449704eabe1048b3
impnguyen <imoney91@me.com>
2018-05-22 17:04:09 +0200

    setup suite

--- pageobjects/Search.page.js
+++ pageobjects/Search.page.js
@@ -1,17 +1,23 @@
-var Page = require ('./Page');
+// login.page.js
+var Page = require('./Page');
 
-class SearchPage extends Page {
+var SearchPage = Object.create(Page, {
+    /**
+     * define elements
+     */
+    searchInput: { get: function () { return browser.element('#lst-ib'); } },
+    searchButton: { get: function () { return browser.element('#sbtc > div.gstl_0.sbdd_a > div:nth-child(2) > div.sbdd_b > div > ul > li:nth-child(11) > div > span:nth-child(1) > span > input'); } },
 
-    get searchInput()  { return browser.element('#lst-ib'); }
+    /**
+     * define or overwrite page methods
+     */
+    open: { value: function() {
+        Page.open.call(this, '/');
+    } },
 
-    open() {
-        super.open('login');
-    }
-
-    submit() {
+    submit: { value: function() {
         this.form.submitForm();
-    }
+    } }
+});
 
-}
-
-module.export = new SearchPage();
+module.exports = SearchPage;
--- specs/Search.spec.js
+++ specs/Search.spec.js
@@ -1,12 +1,19 @@
-var LoginPage = require('../pageobjects/Search.page');
+var expect = require('chai').expect;
+var SearchPage = require('../pageobjects/Search.page');
+var assert = require('assert');
+
 
 describe('search page', function () {
-    it('should deny access with wrong creds', function () {
-        LoginPage.open();
-        LoginPage.searchInput.setValue('test');
-        LoginPage.submit();
+    it('should show results with "test"', function () {
+        SearchPage.open();
+        browser.pause(1000);
+        SearchPage.searchInput.setValue('test');
+        browser.pause(1000);
+        assert.equal(browser.getTitle(), 'Google');
+        SearchPage.searchButton.click();
+        browser.pause(1000);
 
-        expect(LoginPage.flash.getText()).to.contain('Your username is invalid!');
+        assert.equal(browser.getTitle(), 'test - Google-Suche');
     });
 
 });
